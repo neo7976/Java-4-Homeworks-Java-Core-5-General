f46522df3bff226f8a746e185518e99b43d3fbd2
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-02 16:03:02 +0300

    Разбор с лекции json

--- lessonJSON/src/main/java/MainJSON.java
+++ lessonJSON/src/main/java/MainJSON.java
@@ -74,16 +74,25 @@ public class MainJSON {
 //        }
 
 
-        //gson serialize
-        Cat cat = new Cat();
-        cat.name = "Матроскин";
-        cat.age = 5;
-        cat.color = Color.blue.getRGB();
+//        //gson serialize
+//        Cat cat = new Cat();
+//        cat.name = "Матроскин";
+//        cat.age = 5;
+//        cat.color = Color.blue.getRGB();
+//
+//        GsonBuilder builder = new GsonBuilder();
+//        Gson gson = builder.setPrettyPrinting().create();
+//        System.out.println(gson.toJson(cat));
+
+
+        //gson deserialize
+        String jsonText = "{\"name\":\"Мурзик\",\"color\":-16777216,\"age\":9}";
 
         GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.setPrettyPrinting().create();
-        System.out.println(gson.toJson(cat));
-
+        Gson gson = builder.create();
+        Cat cat = gson.fromJson(jsonText, Cat.class);
+        System.out.println("Имя: " + cat.name + "\nВозраст: " + cat.age + "\nЦвет: " +
+                cat.color);
 
     }
 }
