830547790866431ff48997590312ac1a2f10e8bb
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-02 17:09:20 +0300

    Решение проблемы с символами

--- lessonJSON/src/main/java/MainJSON.java
+++ lessonJSON/src/main/java/MainJSON.java
@@ -74,25 +74,28 @@ public class MainJSON {
 //        }
 
 
-//        //gson serialize
-//        Cat cat = new Cat();
-//        cat.name = "Матроскин";
-//        cat.age = 5;
-//        cat.color = Color.blue.getRGB();
-//
-//        GsonBuilder builder = new GsonBuilder();
-//        Gson gson = builder.setPrettyPrinting().create();
-//        System.out.println(gson.toJson(cat));
-
-
-        //gson deserialize
-        String jsonText = "{\"name\":\"Мурзик\",\"color\":-16777216,\"age\":9}";
+        //gson serialize
+        Cat cat = new Cat();
+        cat.name = "Матроскин";
+        cat.age = 5;
+        cat.color = Color.blue.getRGB();
 
         GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        Cat cat = gson.fromJson(jsonText, Cat.class);
-        System.out.println("Имя: " + cat.name + "\nВозраст: " + cat.age + "\nЦвет: " +
-                cat.color);
+        Gson gson = builder.setPrettyPrinting().create(); //используется для более красивой и удобной записи
+//        Gson gson = builder.create();
+        System.out.println(gson.toJson(cat));
+
+
+//        //gson deserialize
+//        String jsonText = "{\"name\":\"Мурзик\",\"color\":-16777216,\"age\":9}";
+//
+//        GsonBuilder builder = new GsonBuilder();
+//        Gson gson = builder.create();
+//        Cat cat = gson.fromJson(jsonText, Cat.class);
+//        System.out.println("Имя: " + cat.name + "\nВозраст: " + cat.age + "\nЦвет: " +
+//                cat.color);
+//
+//        System.out.println(jsonText);
 
 
     }
